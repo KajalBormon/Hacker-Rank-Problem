f9df0dfafb7a8f135a6fb6da9ebe149340832571
KajalBormon <kajalbormon40@gmail.com>
2025-09-25 15:07:05 +0600

    String stream

---
+++ StingStream/string_stream.cpp
@@ -0,0 +1,29 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+	// Complete this function
+    vector<int> newArr;
+    int num;
+    char ch;
+    stringstream ss(str);
+
+    while(ss >> num) {
+        newArr.push_back(num);
+        ss >> ch;
+    }
+    return newArr;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < (int)integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}

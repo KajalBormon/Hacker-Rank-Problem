44edd29118d80bb0d8d46ede8625106023dcb072
KajalBormon <kajalbormon40@gmail.com>
2025-09-27 00:10:02 +0600

    Vector sort

--- Practice/practice.cpp
+++ Practice/practice.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
-#include <sstream>
 #include <string>
+#include <sstream>
+#include <exception>
 using namespace std;
 
+class ErrorException : public exception {
+private: 
+    int num;
+    string message; 
+public: 
+    ErrorException(const string& msg, int n) : num(n) {
+        stringstream ss;
+        ss << msg << " (Error Code: " << num << ")";
+        message = ss.str();
+    }
+
+    const char* what() const noexcept override {
+        return message.c_str();
+    }
+
+    int code() const {   
+        return num;
+    }
+};
+
 int main() {
-    string data = "101 John 3.9";
-
-    stringstream ss(data);
-    int id;
-    string name;
-    float gpa;
-
-    ss >> id >> name >> gpa;
-
-    cout << "ID: " << id << ", Name: " << name << ", GPA: " << gpa << endl;
-    // Output: ID: 101, Name: John, GPA: 3.9
+    try {
+        throw ErrorException("Invalid length detected:", 400);
+    } catch (const ErrorException& e) {
+        cout << "Caught Exception: " << e.what() << endl;
+        cout << "Error code only: " << e.code() << endl;
+    }
 }

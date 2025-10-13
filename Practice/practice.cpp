31c57096665a13a7d54ec3a7cda9275f1df417e3
KajalBormon <kajalbormon40@gmail.com>
2025-10-13 19:50:16 +0600

    Grading students

--- Practice/practice.cpp
+++ Practice/practice.cpp
@@ -4,32 +4,11 @@
 #include <exception>
 using namespace std;
 
-// class ErrorException : public exception {
-// private: 
-//     int num;
-//     string message; 
-// public: 
-//     ErrorException(const string& msg, int n) : num(n) {
-//         stringstream ss;
-//         ss << msg << " (Error Code: " << num << ")";
-//         message = ss.str();
-//     }
-
-//     const char* what() const noexcept override {
-//         return message.c_str();
-//     }
-
-//     int code() const {   
-//         return num;
-//     }
-// };
-
 int main() {
-    int arr[5] = {0,20,30};
-    int n = sizeof(arr)/sizeof(arr[0]);
-
-    for(int i = 0; i < n; i++)
-    {
-        cout << arr[i] << endl;
+    for(int i = 0; i<=10; i++) {
+        if(i > 2) {
+            continue; 
+        }
+        cout << "hi ";
     }
 }
